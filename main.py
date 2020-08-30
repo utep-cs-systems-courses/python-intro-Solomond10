9693aa4b1b7234c524d570125fa50bc3f1224d40
student <solomond10@yahoo.com>
2020-08-30 13:25:05 -0600

    letter check is now working. Counting the number of word occurences is a work in progress

--- main.py
+++ main.py
@@ -2,21 +2,29 @@ print("Hello World")
 
 file = open("declaration.txt","r")
 
-wordDict = {}
+wordDict = {'word': 'value'}
 #print(file.read())
 
 data = file.readlines()
+wordCopy = ""
 
 for line in data:
     for word in line.split():
+        #checking to see if the word has any smybols other than a letter
         for letter in word:
-            if letter <= 'a' and >= 'z' or <= 'A' and >= 'Z':
-                print(letter)
-        #if(word not in wordDict):
-         #   wordDict[len(wordDict)] = (word,1)
+            if letter >= 'a' and letter <= 'z' or letter >= 'A' and letter <= 'Z':
+                wordCopy += letter
+        #print(wordCopy)
+        if(wordCopy not in wordDict):
+            wordDict[len(wordDict)] = (wordCopy,1)
+        else:
+             wordDict[wordCopy] = value + 1
+        wordCopy = ""
+        
     #print(word)
 
-#print(wordDict)
+#print(wordDict[1])
+print(wordDict)
 #words = { 'word': 'When', 'count': 1, }
 
 #print(words)
